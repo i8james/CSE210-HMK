4be25fdc7e07435511a16ab00e91cd7cbd4ada60
i8james <james@shifthome.net>
2026-01-09 16:12:50 -0700

    Refactor Program.cs to prompt for user names and display them in a formatted message

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+        
+        string letter;
+        if (percentage >= 90)
+            letter = "A";
+        else if (percentage >= 80)
+            letter = "B";
+        else if (percentage >= 70)
+            letter = "C";
+        else if (percentage >= 60)
+            letter = "D";
+        else
+            letter = "F";
+        
+        Console.WriteLine($"Your letter grade is: {letter}");
     }
 }
